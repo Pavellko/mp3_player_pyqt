c7d5f4ed090cf6ebc41464a32f1da24e112ee880
Pavellko <pavelkollegov@mail.ru>
2022-09-12 12:46:57 +0300

    v2

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from pygame import mixer
+import os
 
 app = QApplication([])
 ui = uic.loadUi("inter.ui")
@@ -9,6 +10,8 @@ ui = uic.loadUi("inter.ui")
 ui.setWindowTitle('Mp3 Player')
 ui.setFixedSize(350, 500)
 ui.show()
+
+flag = False
 
 def open_file():
     file_name = QFileDialog()
@@ -18,12 +21,38 @@ def open_file():
     ui.listWidget.addItems(song)
 
 def play_song():
+    global flag
     path = ui.listWidget.currentItem().text()
     mixer.init()
     mixer.music.load(path)
     mixer.music.play()
+    flag = True
+    
+def open_folder():
+    directory = QFileDialog.getExistingDirectory()
+    print(directory)
+    if directory:
+        for dirpath, dirnames, filenames in os.walk(directory):
+            for file in filenames:
+                if file.endswith('mp3'):
+                    ui.listWidget.addItem(os.path.join(dirpath, file))
+def pause():
+    global flag
+    if flag:
+        mixer.music.pause()
+        flag = False
+    else:
+        mixer.music.unpause()
+        flag = True
+        
+def stop():
+    mixer.music.stop()
+    
 
 ui.pushButton.clicked.connect(open_file)
+ui.pushButton_3.clicked.connect(open_folder)
 ui.pushButton_2.clicked.connect(play_song)
+ui.pushButton_4.clicked.connect(pause)
+ui.pushButton_5.clicked.connect(stop)
 
 app.exec_()
