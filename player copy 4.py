329f5c7b24e0e6003dc253b52e802edaa75c5f40
Pavellko <pavelkollegov@mail.ru>
2022-09-13 10:30:42 +0300

    good loop copy4

--- player copy 4.py
+++ player copy 4.py
@@ -1,29 +1,41 @@
 import pygame
-# import os
+import os
 
+playlist  = []
+
+def open_folder():
+    global playlist 
+    dir = r'C:\Users\pavel\Music\Test'
+
+    for dirpath, dirnames, filenames in os.walk(dir):
+        for file in filenames:
+            if file.endswith('mp3'):
+                playlist.append(os.path.join(dirpath, file))
+                print(os.path.join(dirpath, file))
+                
+open_folder()        
+        
 pygame.mixer.init()
+pygame.display.init()
 
-# list_song = []
+screen = pygame.display.set_mode ( ( 420 , 240 ) )
 
-# def open_folder():
-#     global list_song
-#     dir = 'C:\\Users\\pavel\\Music\\Рингтоны'
+i = 0
+pygame.mixer.music.load ( playlist[i] )  # Get the first track from the playlist
 
-#     for dirpath, dirnames, filenames in os.walk(dir):
-#         for file in filenames:
-#             if file.endswith('mp3'):
-#                 list_song.append(os.path.join(dirpath, file))
-        
-        
-# open_folder()        
-        
-        
-# for i in list_song:
-#     print(i)
-    
-pygame.mixer.init()
-pygame.mixer.music.load(r"C:\Users\pavel\Music\Рингтоны\8-bit Coffin Dance.mp3")
-pygame.mixer.music.play()
+i += 1
+pygame.mixer.music.queue ( playlist[i] ) # Queue the 2nd song
 
-pygame.quit()
+pygame.mixer.music.set_endevent ( pygame.USEREVENT )    # Setup the end track event
+pygame.mixer.music.play()           # Play the music
+
+running = True
+while running:
+    for event in pygame.event.get():        
+        print(i)
+        if event.type == pygame.USEREVENT:      # A track has ended
+            if len ( playlist ) - i > 1: 
+                i+=1      # If there are more tracks in the queue...
+                pygame.mixer.music.queue ( playlist[i] )          
+             
              
