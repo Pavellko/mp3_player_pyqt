73973f99b0e3679a62efdad4f35c2a03bd2f4c98
Pavellko <pavelkollegov@mail.ru>
2022-09-14 16:11:14 +0300

    volume

--- player.py
+++ player.py
@@ -60,7 +60,19 @@ def stop():
     pygame.mixer.music.stop() 
     
 def close_app():
-    exit()     
+    exit()  
+
+def volum_up():
+    global volume
+    if volume != 1.0 :
+        volume += 0.1
+        pygame.mixer.music.set_volume(volume)     
+
+def volum_down():
+    global volume
+    if volume != 0 :
+        volume -= 0.1
+        pygame.mixer.music.set_volume(volume)          
     
 app = QApplication(sys.argv)
 ui = uic.loadUi("inter.ui")
@@ -68,15 +80,22 @@ ui.setWindowTitle('Mp3 Player')
 ui.setFixedSize(350, 500)
 ui.show()
 
+
+
 pygame.init()
 flag = False
 list_song = []    
-    
+volume = 0.5
+pygame.mixer.music.set_volume(volume)   
+  
 ui.pushButton.clicked.connect(open_file)
 ui.pushButton_3.clicked.connect(open_folder)
 ui.pushButton_2.clicked.connect(play_song)
 ui.pushButton_4.clicked.connect(pause)
 ui.pushButton_5.clicked.connect(stop)
+
+ui.pushButton_6.clicked.connect(volum_up)
+ui.pushButton_7.clicked.connect(volum_down)
 
 app.lastWindowClosed.connect(close_app)
 sys.exit(app.exec_())
