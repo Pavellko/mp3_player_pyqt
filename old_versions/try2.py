ef2381d8af18e320e5766cf7a1a8b40c5cb98c0a
Pavellko <pavelkollegov@mail.ru>
2022-09-18 22:48:17 +0300

    55

---
+++ old_versions/try2.py
@@ -0,0 +1,26 @@
+# import vlc
+# p = vlc.MediaPlayer(r"C:\Users\pavel\Music\Test\01_BloodhoundGang.mp3")
+# p.play()
+
+# import webbrowser
+# webbrowser.open(r"C:\Users\pavel\Music\Test\01_BloodhoundGang.mp3")
+
+# import playsound
+# playsound.playsound(r"C:\Users\pavel\Music\Test\01_BloodhoundGang.mp3")
+
+
+# import pyglet
+# song = pyglet.media.load(r"C:\Users\pavel\Music\Test\01_BloodhoundGang.mp3")
+# song.play()
+# pyglet.app.run()
+
+# from pydub import AudioSegment
+# from pydub.playback import play
+
+# song = AudioSegment.from_mp3('Zombie.mp3')
+# play(song)
+
+# from IPython.display import Audio
+
+# Audio(data='Zombie.mp3', autoplay=False)
+
