f6ff437bc7a47ee6e02d15cf79cf54d1240ca8e6
Pavellko <pavelkollegov@mail.ru>
2022-09-19 11:57:15 +0300

    good

--- player_eve.py
+++ player_eve.py
@@ -10,7 +10,7 @@ from PyQt5.QtGui        import *
 from PyQt5.QtCore       import *
 from PyQt5.QtMultimedia import *
 from PyQt5 import uic
-
+import design
 
 
 
@@ -18,18 +18,8 @@ class App(QWidget):
     def __init__(self):
         super(App, self).__init__()
         self.start()
-        self.ui.label.setStyleSheet("background-image: url(fon.png);")
-        self.ui.pushButton.setStyleSheet("QPushButton#pushButton{\n"
-                                         "background-image: url(knopka.png);\n"
-                                         "font: 75 14pt 'Arial';\n"
-                                         "color: rgb(255, 255, 206);\n"
-                                         "border: none;\n"
-                                         "}\n"
-                                         "QPushButton#pushButton:pressed{\n"
-	                                     "padding-left: 3px;\n"
-	                                     "padding-top: 3px;\n"
-                                         "}\n"                                         
-                                         )
+        # self.ui.label.setStyleSheet("background-image: url(fon.png);")
+        # self.ui.pushButton.setStyleSheet(design.knopka)                                
                                      
     def start(self):
         self.ui = uic.loadUi("inter2.ui")
