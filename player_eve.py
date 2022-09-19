152cbf6a9fcfe48b75ef6565d5c04beab0c5cdc9
Pavellko <pavelkollegov@mail.ru>
2022-09-19 12:50:51 +0300

    last

--- player_eve.py
+++ player_eve.py
@@ -4,15 +4,12 @@ import configparser
 import random
 import time
 
-
 from PyQt5.QtWidgets    import *
 from PyQt5.QtGui        import *
 from PyQt5.QtCore       import *
 from PyQt5.QtMultimedia import *
 from PyQt5 import uic
 import design
-
-
 
 class App(QWidget): 
     def __init__(self):
@@ -22,14 +19,21 @@ class App(QWidget):
         # self.ui.pushButton.setStyleSheet(design.knopka)                                
                                      
     def start(self):
-        self.ui = uic.loadUi("inter2.ui")
+        self.ui = uic.loadUi("inter3.ui")
+        self.ui.pushButton_2.clicked.connect(self.open_folder)
         self.ui.show()
+        
+    def open_folder(self):
+        directory = QFileDialog.getExistingDirectory()
+        if directory:
+            for dirpath, dirnames, filenames in os.walk(directory):
+                for file in filenames:
+                    if file.endswith('mp3'):
+                        self.ui.listWidget.addItem(os.path.join(dirpath, file))    
 
 
 if __name__=='__main__':
 	app = QApplication(sys.argv)
 	ex=App()
 	app.exec_()
-
-
 
